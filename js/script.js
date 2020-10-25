9eecc168b9be9d8bf7b6eb69084900405b2e6a8d
Meenambiga <meena1000aero@gmail.com>
2020-10-25 17:35:09 -0400

    Modified JS Script file

--- js/script.js
+++ js/script.js
@@ -63,15 +63,13 @@ var switchMenuToActive = function () {
 // On page load (before images or CSS)
 document.addEventListener("DOMContentLoaded", function (event) {
 
-// *** start ***
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitly setting the flag to get JSON from server processed into an object literal
+  buildAndShowHomeHTML,
+  true); 
 });
-// *** finish **
 
 
 // Builds HTML for the home page based on categories array
